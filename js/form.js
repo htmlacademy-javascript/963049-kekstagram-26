64281f74c3dfb8d70dfe14aeea9e51ff8fa8a7a9
Enver <enverbelyalov86@gmail.com>
2022-07-16 15:26:07 +0300

    Задание 7.Правда или действие (часть 1)(версия 1)

---
+++ js/form.js
@@ -0,0 +1,102 @@
+import {getRandomArrayElement, isEscapeKey} from './util.js';
+
+const uploadFile = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const uploadCancel = document.querySelector('#upload-cancel');
+
+uploadFile.addEventListener('change', () => {
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      imgUploadOverlay.classList.add('hidden');
+    }
+  });
+});
+
+uploadCancel.addEventListener('click', () => {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+});
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+const hashtagsElement = imgUploadForm.querySelector('.text__hashtags');
+const textDescriptionElement = imgUploadForm.querySelector('.text__description');
+const MAX_AMOUNT_HASHTAGS = 5;
+const MAX_AMOUNT_TEXT_DESCRIPTION = 140;
+
+//комментарий не обязателен
+const validateTextDescriptionNotRequired = (value) => {
+  const descriptionElement = imgUploadForm.querySelector('[name="description"]:required');
+  return textDescriptionElement.value !== descriptionElement.value;
+};
+
+//длина комментария не может составлять больше 140 символов;
+const countLengthDescription = getRandomArrayElement(textDescriptionElement.value, MAX_AMOUNT_TEXT_DESCRIPTION);
+
+//если фокус находится в поле ввода комментария, нажатие на Esc
+// не должно приводить к закрытию формы редактирования изображения.(НЕ РАБОТАЕТ)вариант 1
+const notEscapeTextDescription = () => {
+  if (textDescriptionElement.focus()) {
+    document.addEventListener('keydown', (evt) => {
+      if (isEscapeKey(evt)) {
+        evt.preventDefault();
+        imgUploadOverlay.classList.remove('hidden');
+      }
+    });
+  }
+};
+notEscapeTextDescription();
+
+// не должно приводить к закрытию формы редактирования изображения.(НЕ РАБОТАЕТ)вариант 2
+textDescriptionElement.addEventListener('focus', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    imgUploadOverlay.classList.remove('hidden');
+  }
+});
+
+//валидация поля с хэштегом
+const validateHashtags = (value) => {
+  const hashtags = value.split(' '); //валидация на пробелы между хэштегами
+  const result = hashtags.filter(Boolean);
+  const uniqueHashtags = new Set(hashtags); //валидация на уникальность
+  return hashtags.length === uniqueHashtags.size && result;
+};
+
+const controlHashtagsSymbols = () => {
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1-19}i\s/;
+  validateHashtags().every((item) => re.test(item));
+};
+
+const controlHashtagsAmount = () => validateHashtags().length <= MAX_AMOUNT_HASHTAGS;
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'text__hashtags', // Элемент, на который будут добавляться классы
+  errorClass: 'text__hashtags--invalid', // Класс, обозначающий невалидное поле
+  successClass: 'text__hashtags--valid', // Класс, обозначающий валидное поле
+  errorTextParent: 'text__hashtags', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'p', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'text__hashtags__error' // Класс для элемента с текстом ошибки
+});
+
+pristine.addValidator(hashtagsElement, controlHashtagsSymbols ,
+  'хэш-тег начинается с символа #,' +
+  ' строка после решётки должна состоять из букв и чисел,' +
+  ' хеш-тег должен быть от 2 до 20 символов, ' +
+  'хэш-теги нечувствительны к регистру');
+pristine.addValidator(hashtagsElement, validateHashtags, 'хэш-теги разделяются пробелами,' +
+  'один и тот же хэш-тег не может быть использован дважды' );
+pristine.addValidator(hashtagsElement, controlHashtagsAmount,
+  'нельзя указать больше пяти хэш-тегов');
+pristine.addValidator(textDescriptionElement, validateTextDescriptionNotRequired,
+  'комментарий не обязателен');
+pristine.addValidator(textDescriptionElement, countLengthDescription,
+  'длина комментария не может составлять больше 140 символов');
+
+imgUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {postDesription} from './data.js';
 import './picture.js';
 import './bigpicture.js';
 import './popap.js';
+import './form.js';
 
 // eslint-disable-next-line no-console
 console.log(postDesription());
